ed3ec18a9831e39446c57b4ecf6af3c642912ce2
Thato <developer101@Xbuuntu.myguest.virtualbox.org>
2024-11-14 23:16:48 +0200

    Created operate function

--- app.js
+++ app.js
@@ -4,3 +4,46 @@
 let num1=0;
 let operator;
 let num2=0;
+
+//Functions
+function add(a,b)
+{
+    return a+b;
+}
+
+function subtract(a,b)
+{
+    return a-b;
+}
+
+function multiply(a,b)
+{
+    return a*b;
+}
+
+function divide(a,b)
+{
+    return a/b;
+}
+
+function operate(a,op,b)
+{
+    if (op=="+")
+    {
+        add(a,b);
+    }
+
+    else if(op=="−")
+    {
+        subtract(a,b);
+    }
+    else if(op=="÷")
+    {
+        divide(a,b);
+    }
+    else if(op=="×")
+    {
+        multiply(a,b);
+    }
+
+}
